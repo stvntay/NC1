989f13c94f6ed22528e00eb4d467d15d68b8262a
steven <taysteven111@gmail.com>
2019-05-15 12:16:12 +0700

    Initial Commit

---
+++ NC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1
+//
+//  Created by Steven on 5/15/19.
+//  Copyright © 2019 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
